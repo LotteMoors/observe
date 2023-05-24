fb18a44efc9f60a4562be2d82541de725f36ea23
Lotje <lotte-moors@hotmail.com>
2023-05-24 15:44:31 +0200

    feat: config, packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    env: {
-        NEXT_PUBLIC_PLANT_API_KEY: process.env.NEXT_PUBLIC_PLANT_API_KEY,
-        NEXT_PUBLIC_INSECT_API_KEY: process.env.NEXT_PUBLIC_INSECT_API_KEY,
-        NEXT_PUBLIC_MUSHROOM_KEY: process.env.NEXT_PUBLIC_MUSHROOM_KEY,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost', 'plant-id.ams3.cdn.digitaloceanspaces.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plant-id.ams3.cdn.digitaloceanspaces.com',
+        port: '',
       },
-}
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_PLANT_API_KEY: process.env.NEXT_PUBLIC_PLANT_API_KEY,
+    NEXT_PUBLIC_INSECT_API_KEY: process.env.NEXT_PUBLIC_INSECT_API_KEY,
+    NEXT_PUBLIC_MUSHROOM_KEY: process.env.NEXT_PUBLIC_MUSHROOM_KEY,
+  },
+};
+
+// module.exports = nextConfig;
+
+// const withPWA = require('next-pwa')({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+// })
+
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontWeight: {
+      thin: '100',
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+      black: '900',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
